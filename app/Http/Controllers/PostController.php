1746c1dccc9d62c4492f140fbeebc91145443e9c
et397 <b10812031@crepowermay.com>
2024-05-09 16:45:59 +0800

    增加request類別

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostRequest;
 use App\Repository\PostRepository;
 use Illuminate\Http\Request;
+use Knuckles\Scribe\Attributes\BodyParam;
 use Knuckles\Scribe\Attributes\QueryParam;
 use Knuckles\Scribe\Attributes\Response;
 use Knuckles\Scribe\Attributes\ResponseField;
-use Knuckles\Scribe\Attributes\ResponseFromFile;
+use Knuckles\Scribe\Attributes\UrlParam;
 
 /**
  * @group Post 部落格
@@ -31,13 +33,13 @@ class PostController extends Controller
      * 能自訂欄位降冪或升冪排序
      * 初始為id降冪排序
      */
-    #[QueryParam(name: 'filter', description: '條件查詢', type: 'string', example: 'hello')]
-    #[QueryParam(name: 'orderby', description: '欄位排序', type: 'object', example: '{"id": "desc"}')]
+    #[QueryParam(name: 'filter', description: '條件查詢', type: 'object')]
+    #[QueryParam(name: 'orderby', description: '欄位排序', type: 'string')]
     #[QueryParam(name: 'perpage', description: '每頁顯示的筆數', type: 'int', example: 10)]
     #[QueryParam(name: 'page', description:'頁數', type:'int', example:1)]
-    #[Response( status: 404, description: 'No Content' )]
+    #[Response( status: 200, description: 'No Content' )]
     #[Response( status: 302, description:'Redirect' )]
-    #[ResponseFromFile(status: 200, path: './posts/index.json')]
+    #[Response( status: 200, description: 'OK', content: '{"id":1,"title":"Hello World","content":"Hello World"}')]
     #[ResponseField(name: 'id', description: '文章ID', example: 1)]
     #[ResponseField(name: 'title', description: '文章標題', example: 'Hello World')]
     #[ResponseField(name: 'content', description: '文章內容', example: 'Hello World')]
@@ -53,7 +55,7 @@ class PostController extends Controller
 
         $posts = $this->postRepository->index($perpage, $condition, $order);
 
-        if($posts->isEmpty()){
+        if(empty($posts)){
             return response()->json(['message' => 'No Content'], 204);
         };
         return response()->json($posts);
@@ -71,27 +73,24 @@ class PostController extends Controller
     /**
      * 新增文章
      */
-    #[QueryParam(name: 'title', description: '文章標題', type: 'string', example: 'Hello World')]
-    #[QueryParam(name: 'content', description: '文章內容', type: 'string', example: '早安')]
+    #[BodyParam(name: 'title', description: '文章標題', type: 'string', example: 'Hello World')]
+    #[BodyParam(name: 'content', description: '文章內容', type: 'string', example: '早安')]
     #[Response( status: 200, description: '新增失敗' )]
-    #[ResponseFromFile(status: 201, path: './posts/store.json')]
     #[ResponseField(name: 'id', description: '文章ID', example: 1)]
-    #[ResponseField(name: 'title', description: '文章標題', example: 'Hello World')]
-    #[ResponseField(name: 'content', description: '文章內容', example: '早上好')]
-    public function store(Request  $request)
+    public function store(PostRequest  $request)
     {
 
         //對要求進行驗證
-        $validatedData  = $request->validate([
-            'title' => 'required|max:255|unique:posts',
-            'content' => 'required'
-        ]);
+        // $validatedData  = $request->validate([
+        //     'title' => 'required|max:255|unique:posts',
+        //     'content' => 'required'
+        // ]);
 
         //手動增加'user_id' => 1'的數據
-        $validatedData['user_id'] = 1;
+        $request['user_id'] = 1;
         //從驗證過的要求中取出所有數據並存儲
-        $post = $this->postRepository->store($validatedData);
-
+        $post = $this->postRepository->store($request->all());
+        
 
         // $request->validated();
         if (empty($post)) {
@@ -100,16 +99,17 @@ class PostController extends Controller
 
         // return response()->json(['message' => 'Post not created'], 500);
         //成功回傳成功代碼
-        return response()->json(["id" => $post->id]);
+        // return response()->json(["id" => $post->id]);
+        return response()->json($post);
 
     }
 
     /**
      * 回傳指定文章
      */
-    #[QueryParam(name: 'id', description: '文章ID', type: 'int', example: 1)]
+    #[UrlParam(name: 'id', description: '文章ID', type: 'string', example: 1)]
     #[Response( status: 200, description: 'No Content' )]
-    #[ResponseFromFile(status: 200, path: './posts/show.json')]
+    #[Response( status: 200, description: 'OK', content: '{"id":1,"title":"Hello World","content":"早上好"}')]
     #[ResponseField(name: 'id', description: '文章ID', example: 1)]
     #[ResponseField(name: 'title', description: '文章標題', example: 'Hello World')]
     #[ResponseField(name: 'content', description: '文章內容', example: '早上好')]
@@ -145,24 +145,23 @@ class PostController extends Controller
     /**
      * 修改指定內容
      */
-    #[QueryParam(name: 'id', description: '文章ID', type: 'int', example: 1)]
-    #[QueryParam(name: 'title', description: '文章標題', type: 'string', example: 'Hello World')]
-    #[QueryParam(name: 'content', description: '文章內容', type: 'string', example: '早上好')]
+    #[UrlParam(name: 'id', description: '文章ID', type: 'int', example: 1)]
+    #[BodyParam(name: 'title', description: '文章標題', type: 'string', example: 'Hello World')]
+    #[BodyParam(name: 'content', description: '文章內容', type: 'string', example: '早上好')]
     #[Response( status: 200, description: '修改失敗' )]
-    #[ResponseFromFile(status: 200, path: './posts/update.json')]
     #[ResponseField(name: 'id', description: '文章ID', example: 1)]
     #[ResponseField(name: 'title', description: '文章標題', example: 'Hello World')]
     #[ResponseField(name: 'content', description: '文章內容', example: '早上好')]
-    public function update(Request $request, int $id)
+    public function update(PostRequest $request, int $id)
     {
 
-        $validata = $request->validate([
-            'title' => 'bail|required|max:255',
-            'content' => 'required'
-        ]);
+        // $validata = $request->validate([
+        //     'title' => 'bail|required|max:255',
+        //     'content' => 'required'
+        // ]);
 
         //回傳執行別為boolean
-        $updateresult = $this->postRepository->update($validata, $id);
+        $updateresult = $this->postRepository->update($$request, $id);
 
         if (!$updateresult){
             return response()->json(['message' => 'Post not updated'], 500);
@@ -175,9 +174,8 @@ class PostController extends Controller
     /**
      * 刪除指定文章
      */
-    #[QueryParam(name: 'id', description: '文章ID', type: 'int', example: 1)]
+    #[UrlParam(name: 'id', description: '文章ID', type: 'int', example: 1)]
     #[Response( status: 200, description: '刪除失敗' )]
-    #[ResponseFromFile(status: 200, path: './posts/destroy.json')]
     public function destroy(string $id)
     {
         //
